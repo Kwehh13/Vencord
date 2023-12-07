5366083e19a8e8027034f4d8e2b76d72cf370c15
Kwehh13 <104147635+Kwehh13@users.noreply.github.com>
2023-12-07 17:18:42 +1100

    Create index.ts

---
+++ src/userplugins/kwehh/index.ts
@@ -0,0 +1,14 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "Kwehh Plugins",
+    description: "This plugin is absolutely not epic",
+    authors: [
+        {
+            id: 0n,
+            name: "Kwehh",
+        },
+    ],
+    patches: [],
+    // Delete these two below if you are only using code patches
+});
